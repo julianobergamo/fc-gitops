acbeb964c62b6ef9e13fc0e5f4a144bc466fd37d
julianobergamo <julianobergamo@gmail.com>
2023-03-04 13:59:51 -0400

    add contact

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # fc-gitops
 Curso Full Cycle 3.0 GitOps com algumas alterações
 
-## Na v0.3.0 foi incluído actions do github para o build da imagem docker em alternativa ao passo de build abaixo
+## Na v0.3.0 foi realizado testes com o git flow e branches main/develop protegidos
 
 Veja o readme das outras tags para as orientações correspondentes
--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Hello World!</h1>")
+}
+
+func Contact(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Juliano T. Bergamo <julianobergamo@gmail.com>")
+}
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("<h1>Hello World!</h1>"))
-	})
+	http.HandleFunc("/", Hello)
+	http.HandleFunc("/contact", Contact)
 	http.ListenAndServe(":8080", nil)
 }
